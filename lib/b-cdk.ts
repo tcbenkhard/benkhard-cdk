e213d6d60609b5b08a35808c9f57c39be1a96780
Timo Benkhard <tcbenkhard@gmail.com>
2024-12-23 18:22:22 +0100

    Fix incorrect scope

--- lib/b-cdk.ts
+++ lib/b-cdk.ts
@@ -15,7 +15,7 @@ export class Stack extends cdk.Stack {
             assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
             description: `Service role for ${this.stackName}`,
             managedPolicies: [
-                ManagedPolicy.fromManagedPolicyName(scope, 'LambdaBasicExecutionRole', "AWSLambdaBasicExecutionRole")
+                ManagedPolicy.fromManagedPolicyName(this, 'LambdaBasicExecutionRole', "AWSLambdaBasicExecutionRole")
             ]
         })
         Tags.of(this.serviceRole).add('Application', `${this.stackName}`)
