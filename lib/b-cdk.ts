e94a1c9aa992b89e71c7a6ba0461f93b46b4230a
Timo Benkhard <tcbenkhard@gmail.com>
2024-12-23 15:53:56 +0100

    Add lambda basic permissions

--- lib/b-cdk.ts
+++ lib/b-cdk.ts
@@ -1,6 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from 'constructs';
-import {IRole, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {IRole, ManagedPolicy, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
 import {Tags} from "aws-cdk-lib";
 
 export class Stack extends cdk.Stack {
@@ -13,7 +13,10 @@ export class Stack extends cdk.Stack {
         this.serviceRole = new Role(this, 'ServiceRole', {
             roleName: `${this.stackName}-servicerole`,
             assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
-            description: `Service role for ${this.stackName}`
+            description: `Service role for ${this.stackName}`,
+            managedPolicies: [
+                ManagedPolicy.fromManagedPolicyName(scope, 'LambdaBasicExecutionRole', "AWSLambdaBasicExecutionRole")
+            ]
         })
         Tags.of(this.serviceRole).add('Application', `${this.stackName}`)
     }
